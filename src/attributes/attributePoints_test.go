16aa1f515de03af895a655ae1fc859e894973249
Gabriel Espanhol Santos <gabrielmail09@gmail.com>
2024-10-16 08:38:03 -0300

    test: add tests for AttributePoints.Increase and AttributePoints.Decrease

--- src/attributes/attributePoints_test.go
+++ src/attributes/attributePoints_test.go
@@ -1,8 +1,8 @@
 package attributes
 
 import (
-	"errors"
 	"fmt"
+	"spanishgab/unind/src/errors"
 	"testing"
 )
 
@@ -22,7 +22,9 @@ func TestGet(t *testing.T) {
 	})
 
 	t.Run("Should return an error when the given key does not exist", func(t *testing.T) {
-		expected := errors.New("attribute not found")
+		var expected *errors.InternalError = errors.NewInternalError(
+			fmt.Sprintf(ATTRIBUTE_NOT_FOUND, DEFENSE),
+		)
 		_, got := attributePoints.Get(DEFENSE)
 
 		if got.Error() != expected.Error() {
@@ -68,20 +70,82 @@ func TestHas(t *testing.T) {
 	}
 }
 
-// f'unc TestIncrease(t *testing.T) {
-// 	attributePoints := AttributePoints{
-// 		points: map[Attribute]float64{
-// 			ATTACK: 45,
-// 			DEFENSE: 62,
-// 		},
-// 	}
-// 	t.Run("Should increase an attribute's value", func(t *testing.T) {
-// 		var expected float64 = 65.0
-// 		attributePoints.Increase(DEFENSE, 3)
-// 		got, _ := attributePoints.Get(DEFENSE)
+func TestIncrease(t *testing.T) {
+	attributePoints := AttributePoints{
+		points: map[Attribute]float64{
+			ATTACK: 45,
+			DEFENSE: 62,
+		},
+	}
+	t.Run("Should increase an attribute's value", func(t *testing.T) {
+		var expected float64 = 65.0
+		attributePoints.Increase(DEFENSE, 3)
+		got, _ := attributePoints.Get(DEFENSE)
 
-// 		if got != expected {
-// 			t.Errorf("expected: %g, got %g", expected, got)
-// 		}
-// 	})
-// }'
+		if got != expected {
+			t.Errorf("expected: %g, got %g", expected, got)
+		}
+	})
+
+	t.Run("Should return an error when trying to increase an inexistent attribute", func(t *testing.T) {
+		var expected *errors.InternalError = errors.NewInternalError(
+			fmt.Sprintf(ATTRIBUTE_NOT_FOUND, HEALTH),
+		)
+		got := attributePoints.Increase(HEALTH, 3)
+
+		if got.Error() != expected.Error() {
+			t.Errorf("expected: %q, got %q", expected.Error(), got.Error())
+		}
+	})
+
+	t.Run("Should return an error when the increasing points are less than zero", func(t *testing.T) {
+		var expected *errors.InternalError = errors.NewInternalError(
+			INVALID_INCREASING_VALUE,
+		)
+		got := attributePoints.Increase(ATTACK, -3)
+
+		if got.Error() != expected.Error() {
+			t.Errorf("expected: %q, got %q", expected.Error(), got.Error())
+		}
+	})
+}
+
+func TestDecrease(t *testing.T) {
+	attributePoints := AttributePoints{
+		points: map[Attribute]float64{
+			ATTACK: 45,
+			DEFENSE: 62,
+		},
+	}
+	t.Run("Should decrease an attribute's value", func(t *testing.T) {
+		var expected float64 = 55.0
+		attributePoints.Decrease(DEFENSE, -7)
+		got, _ := attributePoints.Get(DEFENSE)
+
+		if got != expected {
+			t.Errorf("expected: %g, got %g", expected, got)
+		}
+	})
+
+	t.Run("Should return an error when trying to decrease an inexistent attribute", func(t *testing.T) {
+		var expected *errors.InternalError = errors.NewInternalError(
+			fmt.Sprintf(ATTRIBUTE_NOT_FOUND, HEALTH),
+		)
+		got := attributePoints.Decrease(HEALTH, 3)
+
+		if got.Error() != expected.Error() {
+			t.Errorf("expected: %q, got %q", expected.Error(), got.Error())
+		}
+	})
+
+	t.Run("Should return an error when the decreasing points are greater than zero", func(t *testing.T) {
+		var expected *errors.InternalError = errors.NewInternalError(
+			INVALID_DECREASING_VALUE,
+		)
+		got := attributePoints.Decrease(ATTACK, 3)
+
+		if got.Error() != expected.Error() {
+			t.Errorf("expected: %q, got %q", expected.Error(), got.Error())
+		}
+	})
+}
