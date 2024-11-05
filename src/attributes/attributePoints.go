f4809b1ac915caca6c1edbd8333f65dc3f756175
spanishGab <gabrielmail09@gmail.com>
2024-11-05 09:49:46 -0300

    feat: crating constructors for specific AttributePoints and a new method: checkAttributeValues

--- src/attributes/attributePoints.go
+++ src/attributes/attributePoints.go
@@ -7,8 +7,9 @@ import (
 
 const (
 	ATTRIBUTE_NOT_FOUND      = "attribute %q does not exist"
-	INVALID_INCREASING_VALUE = "increasing value must be less than zero"
-	INVALID_DECREASING_VALUE = "decreasing value must be greater than zero"
+	INVALID_ATTRIBUTE_VALUE  = "attribute values must be greater than zero"
+	INVALID_INCREASING_VALUE = "increasing value must be greater than zero"
+	INVALID_DECREASING_VALUE = "decreasing value must be less than zero"
 )
 
 type Attribute string
@@ -22,6 +23,41 @@ const (
 
 type AttributePoints struct {
 	points map[Attribute]float64
+}
+
+func NewBattlePoints(attackPoints, defensePoints float64) (*AttributePoints, *errors.InternalError) {
+	if err := checkAttributeValues(attackPoints, defensePoints); err != nil {
+		return nil, err
+	}
+
+	return &AttributePoints{
+		points: map[Attribute]float64{
+			ATTACK:  attackPoints,
+			DEFENSE: defensePoints,
+		},
+	}, nil
+}
+
+func NewHealthPoints(healthPoints float64) (*AttributePoints, *errors.InternalError) {
+	if err := checkAttributeValues(healthPoints); err != nil {
+		return nil, err
+	}
+	return &AttributePoints{
+		points: map[Attribute]float64{
+			HEALTH: healthPoints,
+		},
+	}, nil
+}
+
+func NewIntelligencePoints(intelligencePoints float64) (*AttributePoints, *errors.InternalError) {
+	if err := checkAttributeValues(intelligencePoints); err != nil {
+		return nil, err
+	}
+	return &AttributePoints{
+		points: map[Attribute]float64{
+			INTELLIGENCE: intelligencePoints,
+		},
+	}, nil
 }
 
 func (ap *AttributePoints) Get(attribute Attribute) (float64, *errors.InternalError) {
@@ -62,5 +98,14 @@ func (ap *AttributePoints) Decrease(attribute Attribute, points float64) *errors
 		return errors.NewInternalError(INVALID_DECREASING_VALUE)
 	}
 	ap.points[attribute] += points
+	return nil
+}
+
+func checkAttributeValues(value ...float64) *errors.InternalError {
+	for _, attributePoint := range value {
+		if attributePoint < 0 {
+			return errors.NewInternalError(INVALID_ATTRIBUTE_VALUE)
+		}
+	}
 	return nil
 }
