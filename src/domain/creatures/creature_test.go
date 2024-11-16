e5082712ccf0d896219c586f8f5e105278b82354
spanishGab <gabrielmail09@gmail.com>
2024-11-16 08:40:02 -0300

    test: add tests for Defend and GetBattlePoints

--- src/domain/creatures/creature_test.go
+++ src/domain/creatures/creature_test.go
@@ -18,20 +18,55 @@ func TestAttack(t *testing.T) {
 	)
 
 	t.Run("Should return the attack points successfully", func(t *testing.T) {
-		var expected float64 = 68.6
+		var expected float64 = 69
 		got := elf.Attack()
 
 		if got != expected {
-			t.Errorf("expected: %.2f, got: %.2f", expected, got)
+			t.Errorf("expected: %f, got: %f", expected, got)
 		}
 	})
+}
 
-	t.Run("Should return the attack points successfully", func(t *testing.T) {
-		var expected float64 = 68.6
-		got := elf.Attack()
+func TestDefend(t *testing.T) {
+	woodenShield, _ := weapons.NewShield("Wooden shield", 10)
+	ironSword, _ := weapons.NewSword("Iron sword", 8, 2)
+	attributePoints, _ := NewCreatureAttributes(90, 70, 85)
+	elf := New(
+		"Legolas",
+		ELF,
+		woodenShield,
+		ironSword,
+		*attributePoints,
+	)
+
+	t.Run("Should return the defense points successfully", func(t *testing.T) {
+		var expected float64 = 68
+		got := elf.Defend()
 
 		if got != expected {
-			t.Errorf("expected: %.2f, got: %.2f", expected, got)
+			t.Errorf("expected: %f, got: %f", expected, got)
+		}
+	})
+}
+
+func TestGetBattlePoints(t *testing.T) {
+	woodenShield, _ := weapons.NewShield("Wooden shield", 10)
+	ironSword, _ := weapons.NewSword("Iron sword", 8, 2)
+	attributePoints, _ := NewCreatureAttributes(90, 70, 85)
+	elf := New(
+		"Legolas",
+		ELF,
+		woodenShield,
+		ironSword,
+		*attributePoints,
+	)
+
+	t.Run("Should return the battle points successfully, no matter if attack or defense points", func(t *testing.T) {
+		var expected float64 = 68
+		got := elf.getBattlePoints(12)
+
+		if got != expected {
+			t.Errorf("expected: %f, got: %f", expected, got)
 		}
 	})
 }
