c33bee6e73e5233356edc353c55daace7280450c
spanishGab <gabrielmail09@gmail.com>
2024-12-17 07:30:44 -0300

    fix: refactoryuing creature's facotries

--- src/domain/creatures/errors.go
+++ src/domain/creatures/errors.go
@@ -6,6 +6,10 @@ var InvalidAttributeValueError *errors.InternalError = errors.NewInternalError(
 	"attribute values must be greater than zero",
 )
 
+var MismatchedWeaponTypeError *errors.InternalError = errors.NewInternalError(
+	"the weapon type does not match with the expected initial weapon type for this character",
+)
+
 var CreatureDiedError *errors.DomainError = errors.NewDomainError(
 	"creature-died",
 	"creature died",
--- src/domain/creatures/factory.go
+++ src/domain/creatures/factory.go
@@ -1,22 +1,99 @@
 package creatures
 
-import "spanishgab/unind/src/domain/weapons"
+import (
+	"spanishgab/unind/src/domain/weapons"
+	"spanishgab/unind/src/errors"
+)
 
-func NewHuman(name string) *Creature {
-	sword, _ := weapons.NewSword("Iron sword", 7, 2)
-	shield, _ := weapons.NewShield("Wooden shield", 7)
-	attributes, _ := NewCreatureAttributes(75, 85, 80)
-	return NewCreature(name, Human, sword, shield, *attributes)
+func NewHuman(
+	name string,
+	sword *weapons.Weapon,
+	shield *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if sword.GetType() != weapons.Sword {
+		return nil, MismatchedWeaponTypeError
+	}
+	if shield.GetType() != weapons.Shield {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Human, sword, shield, attributes), nil
 }
 
-func NewWizard(name string) *Creature {
-	sword, _ := weapons.NewStaff("Wooden staff", 10, 10)
-	attributes, _ := NewCreatureAttributes(90, 95, 85)
-	return NewCreature(name, Human, sword, nil, *attributes)
+func NewWizard(
+	name string,
+	staff *weapons.Weapon,
+	sword *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if staff.GetType() != weapons.Staff {
+		return nil, MismatchedWeaponTypeError
+	}
+	if sword.GetType() != weapons.Sword {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Wizard, staff, sword, attributes), nil
 }
 
-func NewElf(name string) *Creature {
-	sword, _ := weapons.NewStaff("Wooden staff", 10, 10)
-	attributes, _ := NewCreatureAttributes(90, 95, 85)
-	return NewCreature(name, Human, sword, nil, *attributes)
+func NewElf(
+	name string,
+	bowAndArrow *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if bowAndArrow.GetType() != weapons.BowAndArrow {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Elf, bowAndArrow, nil, attributes), nil
+}
+
+func NewDwarf(
+	name string,
+	axe *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if axe.GetType() != weapons.Axe {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Dwarf, axe, nil, attributes), nil
+}
+
+func NewOrc(
+	name string,
+	sword *weapons.Weapon,
+	shield *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if sword.GetType() != weapons.Sword {
+		return nil, MismatchedWeaponTypeError
+	}
+	if shield.GetType() != weapons.Shield {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Orc, sword, shield, attributes), nil
+}
+
+func NewSorcerer(
+	name string,
+	wand *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if wand.GetType() != weapons.Wand {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Orc, wand, nil, attributes), nil
+}
+
+func NewShadowLord(
+	name string,
+	staff *weapons.Weapon,
+	sword *weapons.Weapon,
+	attributes CreatureAttributes,
+) (*Creature, *errors.InternalError) {
+	if staff.GetType() != weapons.Staff {
+		return nil, MismatchedWeaponTypeError
+	}
+	if sword.GetType() != weapons.Sword {
+		return nil, MismatchedWeaponTypeError
+	}
+	return NewCreature(name, Orc, staff, sword, attributes), nil
 }
